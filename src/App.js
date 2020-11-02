e9241536c94752b1cf6bedd5ef703e1b781ec229
Quang Thuan <thuanquang2009@gmail.com>
2020-11-02 23:24:20 +0700

    Change Title

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Luu Quang Thuan
-        </p>
-      </header>
+      <h1>Luu Quang Thuan</h1>
     </div>
   );
 }
